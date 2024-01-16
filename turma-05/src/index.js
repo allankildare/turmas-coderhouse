5145ef25520f59bab8a46564a91a6a9ec9dab64b
allankildare <allankildaredev@gmail.com>
2024-01-16 00:37:44 -0300

    chore: configuracao do firebase

--- turma-05/src/index.js
+++ turma-05/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCzZBUJMz3rGms5euNTHZPttxGH7HXI60",
+  authDomain: "coderhouse-50925.firebaseapp.com",
+  projectId: "coderhouse-50925",
+  storageBucket: "coderhouse-50925.appspot.com",
+  messagingSenderId: "629181470921",
+  appId: "1:629181470921:web:6d7e83980587e04884d518"
+}
+
+initializeApp(firebaseConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />)
