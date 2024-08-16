d208c289bac177048172db06cecda8f7df63d86a
Allan Kildare <allankildaredev@gmail.com>
2024-08-15 21:43:54 -0300

    refactor: comentarios relevantes para estudantes

--- turma-06/aula-02/TicketManager.js
+++ turma-06/aula-02/TicketManager.js
@@ -1,14 +1,16 @@
 class TicketManager {
-  #basePrice = 0.15;
+  #basePrice = 0.15; // propriedade privada
 
   constructor() {
+    // no constructor definimos as propriedades publicas da classe
     this.events = [];
     this.nextId = 1;
   }
 
+  // addEvent é um exemplo de método da classe
   addEvent(nome, lugar, preco, capacidade = 50, data = new Date()) {
     const novoEvento = {
-      id: this.nextId++,
+      id: this.nextId++, // incrementa o id
       nome: nome,
       lugar: lugar,
       preco: preco + this.#basePrice,
@@ -16,16 +18,16 @@ class TicketManager {
       data: data,
       participants: [],
     };
-    this.events.push(novoEvento);
+    this.events.push(novoEvento); // adiciona o evento ao array de eventos
   }
 
-  // Getter
+  // Getter para os eventos
   getEvents() {
     return this.events
   }
 }
 
-const manager = new TicketManager()
+const manager = new TicketManager() // criação da instância de TicketManager
 manager.addEvent("Concerto de Rock", "Estádio", 100)
 manager.addEvent("Peça de Teatro", "Teatro Municipal", 50, 100)
 console.log(manager.getEvents())
