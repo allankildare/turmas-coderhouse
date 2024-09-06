ef3b235f1f3c22a1b827628ca837f6309ca956bc
Allan Kildare <allankildaredev@gmail.com>
2024-09-05 21:47:57 -0300

    feat: aula 10

--- turma-06/aula-09/src/app.js
+++ turma-06/aula-09/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import handlebars from 'express-handlebars'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { Server } from 'socket.io' // import do Socket.io
 
 // routes
 import viewsRouter from './routes/view.js'
@@ -28,6 +29,25 @@ app.use('/', viewsRouter)
 app.use(express.static(publicPath))
 
 // inicializar o servidor
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
     console.log(`Servidor rodando em:\nhttp://localhost:${PORT}`)
+})
+
+// Criação de um socket server
+const socketServer = new Server(httpServer)
+
+socketServer.on('connection', (socket) => {
+    console.log('Novo cliente conectado')
+    const arrMessages = []
+    socket.on('message', (data) => {
+        console.log(data)
+        arrMessages.push(data)
+        socket.emit('event_for_individual_socket', arrMessages)
+    })
+
+    socket.emit('event_for_individual_socket', 'Esta mensagem só deve ser recebida pelo socket do cliente')
+
+    socket.broadcast.emit('event_for_everyone_but_the_current_socket', 'Este evento será visto por todos os sockets conectados, EXCETO o socket atual de onde a mensagem foi enviada')
+
+    socket.emit('event_for_all', 'Enviada para todos')
 })
--- turma-06/aula-09/src/public/scripts/index.js
+++ turma-06/aula-09/src/public/scripts/index.js
@@ -1,1 +1,38 @@
+// elements
+const messagesUl = document.getElementById('messages')
+const chatInput = document.getElementById('chat')
+const btnEnviar = document.getElementById('btnEnviar')
+
 console.log('Você está vendo uma página gerada usando Handlebars')
+
+// inicializacao do socket
+const socket = io()
+
+// configuração do cliente
+socket.on('event_for_individual_socket', (data) => {
+    console.log('Mensagem recebida:\n', data)
+    
+    if (Array.isArray(data)) {
+        messagesUl.innerHTML = '' // limpa conteúdo do elemento UL
+
+        // adiciona LIs com todas as messagens
+        data.forEach((item) => {
+            const newItem = document.createElement('li')
+            newItem.textContent = item.message
+            messagesUl.appendChild(newItem)
+        })
+    }
+})
+
+socket.on('event_for_all', (data) => {
+    console.log('Mensagem recebida:\n', data)
+})
+
+socket.on('event_for_everyone_but_the_current_socket', (data) => {
+    console.log('Mensagem recebida:\n', data)
+})
+
+btnEnviar.addEventListener('click', () => {
+    const value = chatInput.value
+    socket.emit('message', { socketId: socket.id, message: value })
+})
