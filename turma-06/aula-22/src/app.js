53c53c550dd940bc6e2d52a9b8903134afe9a756
Allan Kildare <allankildaredev@gmail.com>
2024-10-24 20:30:13 -0300

    feat: aula 22

---
+++ turma-06/aula-22/src/app.js
@@ -0,0 +1,38 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
+import { config } from 'dotenv'
+
+config()
+
+const PORT = process.env.PORT || 8080
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const publicPath = path.join(__dirname, 'public')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.use(express.static(publicPath))
+
+app.post('/login', (request, response) => {
+  const { email, password } = request.body
+  let token = jwt.sign({ email, password }, process.env.CODER_SECRET, {
+    expiresIn: '24h',
+  })
+
+  return response
+    .cookie('coderCookieToken', token, {
+      maxAge: 60 * 60 * 1000,
+    })
+    .send({ status: 'success', message: 'Logado com sucesso!' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando!\nAcesse http://localhost:${PORT}/`)
+})
